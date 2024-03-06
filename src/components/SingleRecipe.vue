fdf93e760d1a01354754e5fd9f6e4b9b01a4d2cf
Christian Dickreuter <christian.dickreuter@gmail.com>
2024-03-06 10:20:43 +0100

    Implemented single recipe

---
+++ src/components/SingleRecipe.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps({
+  recipe: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+<template>
+  <h2>{{ recipe.title }}</h2>
+  <p>{{ recipe.kindOfRecipe }}</p>
+  <ul>
+    <li v-for="ingredient in recipe.ingredients" :key="ingredient">
+      {{ ingredient }}
+    </li>
+  </ul>
+</template>
