aa4b3340741726eb09e59afc44a420ad77fc1cb5
Christian Dickreuter <christian.dickreuter@gmail.com>
2024-03-06 10:20:25 +0100

    Implemented recipe list

---
+++ src/components/RecipeList.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import SingleRecipe from './SingleRecipe.vue'
+
+defineProps({
+  recipes: {
+    type: Array,
+    required: true
+  }
+})
+</script>
+<template>
+  <div class="recipe-list">
+    <div v-for="recipe in recipes" :key="recipe.id">
+       <SingleRecipe :recipe="recipe" />
+    </div>
+  </div>
+</template>
+
+<style>
+
+</style>
